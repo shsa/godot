80f0b3d4904c547b294fb511d6268fbaf81761b6
shsa <shilovsa@gmail.com>
2023-11-17 13:28:30 +0700

    .

--- Squarez/assets/scripts/rocket.gd
+++ Squarez/assets/scripts/rocket.gd
@@ -2,7 +2,7 @@ extends CubeBase
 
 class_name Rocket
 
-@onready var _particles := $particles
+@onready var _particles := $model/particles
 
 func init():
 	rotation = Vector3(0, randi_range(0, 3) * PI / 2, 0)
